2fb76272b1a13f0b9617409e8b408995c6afaec8
Caseyann <goore_44@hotmail.ca>
2022-10-18 02:15:12 -0400

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Caseyann Goore on 2022-10-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
